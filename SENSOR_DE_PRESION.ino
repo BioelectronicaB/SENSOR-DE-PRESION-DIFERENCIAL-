9dedca0ae06a02be6b675a5dc1528cb068770b58
Bioelectronica Se escribe con B <35161121+BioelectronicaSeEscribeConB@users.noreply.github.com>
2018-01-09 22:36:45 -0600

    Add files via upload
    SALUDOS

---
+++ SENSOR_DE_PRESION.ino
@@ -0,0 +1,22 @@
+int sensor=A0;
+int valorSensor;
+int presion;
+
+void setup() 
+{
+Serial.begin(9600);
+pinMode(sensor,INPUT);
+}
+
+void loop() 
+{
+valorSensor=analogRead(sensor);
+////CONVERSION A KPA//////
+///// 0-50kPa (0-7.25psi) = 0 a 40mV    0.8 mV/kPa
+presion=map(valorSensor, 0, 180, 0, 50);
+Serial.print("Presion:");
+Serial.print(presion);
+Serial.println(" Kpa");
+delay(200);
+
+}
